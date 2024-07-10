2586471ed439ebdb93b47a4609e951f2c5bd7fb4
AdryGiuliani <giulianiadriano3@gmail.com>
2024-07-10 17:05:52 +0200

    Initial commit

---
+++ src/main/java/com/piattaforme/edilgiulianiback/EdilGiulianibackApplication.java
@@ -0,0 +1,13 @@
+package com.piattaforme.edilgiulianiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdilGiulianibackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdilGiulianibackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piattaforme/edilgiulianiback/EdilGiulianibackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piattaforme.edilgiulianiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdilGiulianibackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
